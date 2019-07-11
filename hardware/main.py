9fd3675861d3544ad27ecf05148b08b84a2d37aa
Phitchawat Lukkanathiti <me@shayennn.com>
2019-07-11 11:46:23 +0700

    Finished btnMon

--- hardware/main.py
+++ hardware/main.py
@@ -1,1 +1,18 @@
 from machine import Pin, PWM, ADC
+from time import sleep
+
+GATESTATUS = False
+
+
+def btnMon():
+    global GATESTATUS
+    p_mon = Pin(34, Pin.IN)
+    btnstatus = False
+    while True:
+        if p_mon.value() == 0:  # BTN-Down
+            if not btnstatus:
+                GATESTATUS = not GATESTATUS
+            btnstatus = True
+        else:
+            btnstatus = False
+        sleep(0.0001)
